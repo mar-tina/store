4eba693c346cc1ec1b0f7d54f17e170809e3fe7b
mar-tina <marr.tina344@gmail.com>
2022-07-26 13:57:14 +0300

    Initial Commit

---
+++ store/storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  storeApp.swift
+//  store
+//
+//  Created by martina khaemba on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
